0c307ab14358825ee4d482cdba567e36d9758159
IvchenkoEV <pesok72@mail.ru>
2025-03-23 21:38:08 +0500

    добавлены проперти

--- telemetry/aggregator/src/main/java/ru/yandex/practicum/AggregationStarter.java
+++ telemetry/aggregator/src/main/java/ru/yandex/practicum/AggregationStarter.java
@@ -8,6 +8,7 @@ import org.apache.kafka.clients.consumer.KafkaConsumer;
 import org.apache.kafka.clients.producer.KafkaProducer;
 import org.apache.kafka.clients.producer.ProducerRecord;
 import org.apache.kafka.common.errors.WakeupException;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 import ru.yandex.practicum.kafka.telemetry.event.SensorEventAvro;
 import ru.yandex.practicum.kafka.telemetry.event.SensorsSnapshotAvro;
@@ -26,7 +27,9 @@ public class AggregationStarter {
     private final KafkaConsumer<String, SensorEventAvro> consumer;
     private final KafkaProducer<String, SensorsSnapshotAvro> producer;
     private final SnapshotStorage snapshotStorage;
+    @Value("${kafka.input-topic}")
     private final String inputTopic;
+    @Value("${kafka.output-topic}")
     private final String outputTopic;
 
     /**
