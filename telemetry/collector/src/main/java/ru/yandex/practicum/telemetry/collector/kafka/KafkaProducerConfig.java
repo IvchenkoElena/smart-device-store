2c84cacd89915d358edeac89410b1b8f96160d6c
IvchenkoEV <pesok72@mail.ru>
2025-03-23 17:59:52 +0500

    Перенесен сериалайзер

--- telemetry/collector/src/main/java/ru/yandex/practicum/telemetry/collector/kafka/KafkaProducerConfig.java
+++ telemetry/collector/src/main/java/ru/yandex/practicum/telemetry/collector/kafka/KafkaProducerConfig.java
@@ -9,6 +9,7 @@ import org.apache.kafka.common.serialization.StringSerializer;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import serializer.GeneralAvroSerializer;
 
 import java.util.Properties;
 
