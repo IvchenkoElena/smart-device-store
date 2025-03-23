1d8d13f16c17c9200578e5737dfe2e777a87bc77
IvchenkoEV <pesok72@mail.ru>
2025-03-23 22:09:34 +0500

    fix

--- telemetry/serialization/avro-schemas/src/main/java/deserializer/SensorsSnapshotDeserializer.java
+++ telemetry/serialization/avro-schemas/src/main/java/deserializer/SensorsSnapshotDeserializer.java
@@ -1,8 +1,10 @@
-//package deserializer;
-//
-//
-//public class SensorsSnapshotDeserializer extends BaseAvroDeserializer<SensorsSnapshotAvro> {
-//    public SensorsSnapshotDeserializer() {
-//        super(SensorsSnapshotAvro.getClassSchema());
-//    }
-//}
+package deserializer;
+
+
+import ru.yandex.practicum.kafka.telemetry.event.SensorsSnapshotAvro;
+
+public class SensorsSnapshotDeserializer extends BaseAvroDeserializer<SensorsSnapshotAvro> {
+    public SensorsSnapshotDeserializer() {
+        super(SensorsSnapshotAvro.getClassSchema());
+    }
+}
