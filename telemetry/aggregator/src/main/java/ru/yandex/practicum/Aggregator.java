1c4f7ce49c911c6e6c87375de67f009bc9f2c5a4
IvchenkoEV <pesok72@mail.ru>
2025-03-23 23:39:03 +0500

    fix2

--- telemetry/aggregator/src/main/java/ru/yandex/practicum/Aggregator.java
+++ telemetry/aggregator/src/main/java/ru/yandex/practicum/Aggregator.java
@@ -7,12 +7,12 @@ import org.springframework.context.ConfigurableApplicationContext;
 
 @SpringBootApplication
 @ConfigurationPropertiesScan
-public class AggregatorApp {
+public class Aggregator {
     public static void main(String[] args) {
         // Запуск Spring Boot приложения при помощи вспомогательного класса SpringApplication
         // метод run возвращает назад настроенный контекст, который мы можем использовать для
         // получения настроенных бинов
-        ConfigurableApplicationContext context = SpringApplication.run(AggregatorApp.class, args);
+        ConfigurableApplicationContext context = SpringApplication.run(Aggregator.class, args);
 
         // Получаем бин AggregationStarter из контекста и запускаем основную логику сервиса
         AggregationStarter aggregator = context.getBean(AggregationStarter.class);
