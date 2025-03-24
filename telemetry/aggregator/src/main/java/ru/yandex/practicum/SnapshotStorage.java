b07263d438c5393e60bdd83c04fb3f51f18359df
IvchenkoEV <pesok72@mail.ru>
2025-03-24 08:55:25 +0500

    fix5

--- telemetry/aggregator/src/main/java/ru/yandex/practicum/SnapshotStorage.java
+++ telemetry/aggregator/src/main/java/ru/yandex/practicum/SnapshotStorage.java
@@ -13,49 +13,33 @@ import java.util.Optional;
 @Slf4j
 @Component
 public class SnapshotStorage {
-    private final Map<String, SensorsSnapshotAvro> snapshotsMap = new HashMap<>();
+    private final Map<String, SensorsSnapshotAvro> snapshots = new HashMap<>();
 
     public Optional<SensorsSnapshotAvro> updateState(SensorEventAvro event) {
-//        Проверяем, есть ли снапшот для event.getHubId()
-//        Если снапшот есть, то достаём его
-//        Если нет, то созадём новый
-        SensorsSnapshotAvro snapshot;
-        if (!snapshotsMap.containsKey(event.getHubId())) {
+        SensorsSnapshotAvro snapshot = snapshots.get(event.getHubId());
+        if (snapshot == null) {
             snapshot = new SensorsSnapshotAvro();
             snapshot.setHubId(event.getHubId());
+            snapshot.setTimestamp(event.getTimestamp());
             snapshot.setSensorsState(new HashMap<>());
-        } else {
-             snapshot = snapshotsMap.get(event.getHubId());
-            //        Проверяем, есть ли в снапшоте данные для event.getId()
-//        Если данные есть, то достаём их в переменную oldState
-            if (snapshot.getSensorsState().containsKey(event.getId())) {
-                SensorStateAvro oldState = snapshot.getSensorsState().get(event.getId());
-//        Проверка, если oldState.getTimestamp() произошёл позже, чем
-//        event.getTimestamp() или oldState.getData() равен
-//        event.getPayload(), то ничего обновлять не нужно, выходим из метода
-//        вернув Optional.empty()
-                if (oldState.getTimestamp().isAfter(event.getTimestamp()) || oldState.getData().equals(event.getPayload())) {
-                    log.debug("Обновление снапшота не требуется для hubId={}, sensorId={}, timestamp={}",
-                            event.getHubId(), event.getId(), event.getTimestamp());
-                    return Optional.empty();
-                }
+        }
+
+        SensorStateAvro oldState = snapshot.getSensorsState().get(event.getId());
+        if (oldState != null) {
+            // Если oldState произошёл позже, чем event
+            // или данные совпадают, то обновление не требуется
+            if (oldState.getTimestamp().isAfter(event.getTimestamp()) ||
+                    oldState.getData().equals(event.getPayload())) {
+                return Optional.empty();
             }
         }
-        // если дошли до сюда, значит, пришли новые данные и
-        // снапшот нужно обновить
+
         SensorStateAvro newState = new SensorStateAvro();
         newState.setTimestamp(event.getTimestamp());
         newState.setData(event.getPayload());
-
         snapshot.getSensorsState().put(event.getId(), newState);
         snapshot.setTimestamp(event.getTimestamp());
-        log.debug("Состояние обновлено для sensorId={}, новое состояние={}",
-                event.getId(), newState);
-
+        snapshots.put(event.getHubId(), snapshot);
         return Optional.of(snapshot);
-//        Создаём экземпляр SensorStateAvro на основе данных события
-//        Добавляем полученный экземпляр в снапшот
-//        Обновляем таймстемп снапшота таймстемпом из события
-//        Возвращаем снапшот - Optional.of(snapshot)
     }
 }
